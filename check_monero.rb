c23f6ed6b52fdb1159d228d18ce8e3efbd835b4d
Giancarlo Franchini <gfranchini@greathealthworks.com>
2017-06-13 11:54:01 -0400

    dryed up the code and configured different spinner

--- check_monero.rb
+++ check_monero.rb
@@ -1,15 +1,14 @@
 #!/usr/local/bin/ruby
 require 'httparty'
 require 'tty-spinner'
+require 'nokogiri'
 
 url = 'https://monerohash.com/api/stats_address?address=45f25xoWSUvPdvDXCQjmEv7ihEceXF6nsVeyNtbavtmFQPgoUAtnKpDgX5w4jHV9dLizRVXLEkuGwBnaTMjDFwq4NTwgkM4&longpoll=true'
-puts "Gathering data..."
-
 stats = nil
-spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :pulse_2)
+spinner = TTY::Spinner.new("[:spinner] Gathering data ...", format: :classic)
 
 spinner.run('Done!') do
-  stats = HTTParty.get('https://monerohash.com/api/stats_address?address=45f25xoWSUvPdvDXCQjmEv7ihEceXF6nsVeyNtbavtmFQPgoUAtnKpDgX5w4jHV9dLizRVXLEkuGwBnaTMjDFwq4NTwgkM4&longpoll=true')
+  stats = HTTParty.get(url)
 end
 
 balance = (stats['stats']['balance'].to_f)/1000000000000
